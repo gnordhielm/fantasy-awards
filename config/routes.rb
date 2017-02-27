510eefd7e32d181f5e42c853f7d8feeeb4a34d5f
Gus Nordhielm <gnordhielm@gmail.com>
2017-02-27 11:10:32 -0800

    set up static pages with controller and views only

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#welcome'
+  get  '/welcome', to: 'static_pages#welcome'
+  get  '/about', to: 'static_pages#about'
+  get  '/help', to: 'static_pages#help'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
