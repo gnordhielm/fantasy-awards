b98aa2127585887f93ba840971da0b46c4c4b238
Gus Nordhielm <gnordhielm@gmail.com>
2017-03-02 20:52:31 -0800

    style commish dashboard

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,2 +1,16 @@
 
 ///// New League Form Interactivity /////
+
+// league-name changer
+$('#league_league_name').focusout(function(e){
+	var leagueName = $('#league_league_name').val()
+	if (!!leagueName) $('.league-name').text(leagueName)
+})
+// award-name changer
+$('#league_award_name').focusout(function(e){
+	var awardName = $('#league_award_name').val()
+	console.log(awardName)
+	if (!!awardName) $('.award-name').html(awardName)
+})
+
+///// Copy Link /////
