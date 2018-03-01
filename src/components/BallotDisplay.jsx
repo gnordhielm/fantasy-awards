ec7a7316ee7961244990d7531d7b133739652a6e
Gus Nordhielm <gnordhielm@gmail.com>
2018-03-01 10:59:09 -0800

    add temp message to ballot summary

--- src/components/BallotDisplay.jsx
+++ src/components/BallotDisplay.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const BallotDisplay = ({ ballot }) => (
   <div className="ballot-display">
     <p>
-      {ballot.bigOne.film}
-      {ballot.bigOne.pointsOn}
+      Ballot summary coming soon...
+      {/* {ballot.bigOne.film}
+      {ballot.bigOne.pointsOn} */}
     </p>
   </div>
 )
